bf143cfac973230578691c52797f695add0cae5a
Dimitar <dimitar.mandarliev@gmail.com>
2023-06-12 22:02:05 +0300

    added useUser hook

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -5,7 +5,7 @@ import {
   useUser as useSupaUser,
 } from "@supabase/auth-helpers-react";
 import { access } from "fs";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
 type UserContextType = {
   accessToken: string | null;
@@ -78,4 +78,13 @@ export const MyUserContextProvider = (props: Props) => {
   };
 
   return <UserContext.Provider value={value} {...props} />;
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUser must be used within a MyUserContextProvider");
+  }
+
+  return context;
 };
